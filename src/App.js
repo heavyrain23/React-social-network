93697d82d98e5c50c56711b93d9c377b208ef2c0
Alex <failemotions95@gmail.com>
2019-10-30 20:21:12 +0200

    container component was added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Link, BrowserRouter} from 'react-router-dom'
 import Header from'./components/Header/Header';
 import Navbar from'./components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
 
 
 const App = (props) => {
@@ -15,11 +15,9 @@ const App = (props) => {
             <Navbar />
             <div className = "app-wrapper-content">
                 <Route path='/Dialogs' 
-                    render={ () => <Dialogs store={props.store} /> } />
+                    render={ () => <DialogsContainer store={props.store} /> } />
                 <Route path='/Profile'
-                     render={ () => <Profile 
-                        profilePage={props.state.profilePage} 
-                        dispatch={props.dispatch}  /> } />
+                     render={ () => <Profile store ={props.store}  /> } />
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
 
@@ -10,29 +9,25 @@ let postsElements =
 
 let newPostElement = React.createRef();
 
-let addPost = () => {
-  props.dispatch(addPostActionCreator());
-  
+let onAddPost = () => {
+  props.addPost();
 }
 
 let onPostChange = () => {
     let text = newPostElement.current.value;
-    // let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text};
-    let action = updateNewPostTextActionCreator(text);
-    props.dispatch(action);
-  
+    props.updateNewPostText(text);
 }
 
   return (
   <div className={s.postsBlock}>
-   <h3> My posts </h3>
+    <h3> My posts </h3>
     <div>
         <div>
             <textarea onChange = {onPostChange} ref={newPostElement}
                       value = {props.newPostText} />
         </div>
         <div>
-            <button onClick = { addPost } >Add post</button>
+            <button onClick = { onAddPost } >Add post</button>
         </div>
      </div>
 
