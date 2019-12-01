c4e593f8ea8a214c9eb446ead3af7a7cc035be78
Alex <failemotions95@gmail.com>
2019-12-01 23:57:43 +0200

    profile data

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,8 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import userPhoto from "../../../assets/images/user.png";
+
+
 const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto }) => {
 
     if (!profile) {
@@ -20,10 +22,41 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto }) => {
             <div className={s.descriptionBlock}>
                 <img src={profile.photos.large || userPhoto} className={s.mainPhoto} />
                 { isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
+
+                <ProfileData profile = {profile}/>
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
     )
+}
+
+const ProfileData = ({ profile }) => {
+    return (
+        <div>
+            <div>
+                <b>Fullname</b>: {profile.fullName}
+            </div>
+            <div>
+                <b>Looking for a job</b>: {profile.lookingForAJob ? 'yes' : 'no'}
+            </div>
+            {profile.lookingForAJob &&
+                <div>
+                    <b>My professional skills</b>: {profile.lookingForAJobDescription}
+                </div>
+            }
+            <div>
+                <b>About me</b>: {profile.aboutMe}
+            </div>
+            <div>
+                <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                    return <Contact key={key} contactTitle={key} contactValue={profile.contact[key]} />
+                })}
+            </div>
+        </div>)
+}
+
+const Contact = ({ contactTitle, contactValue }) => {
+    return <div><b>{contactTitle}</b>: {contactValue}</div>
 }
 
 export default ProfileInfo;
