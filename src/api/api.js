680ff8e3952c34ddeebf9d0a98433bfa7ba24b61
Alex <failemotions95@gmail.com>
2019-12-05 14:40:48 +0200

    profile edit mode was upgreaded

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "3d37f6a3-c0fc-407a-bc64-64abd36c2415"
     }
 });
 
@@ -48,6 +48,9 @@ export const profileAPI = {
                 'Content-Type': 'multipart/form-data'
             }
         });
+    },
+    saveProfile(profile) {
+        return instance.put('profile', profile);
     }
 }
 
