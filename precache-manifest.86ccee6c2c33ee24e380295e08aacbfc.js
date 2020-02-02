a9f382152083d6c691f9da9e57399a4ad8166362
Alex <failemotions95@gmail.com>
2020-02-02 16:27:23 +0200

    Updates

---
+++ precache-manifest.86ccee6c2c33ee24e380295e08aacbfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89604439f533d22d46a578531ab655b1",
+    "url": "/React-social-network/index.html"
+  },
+  {
+    "revision": "263fab3169b209f43bb3",
+    "url": "/React-social-network/static/css/main.13b4c16b.chunk.css"
+  },
+  {
+    "revision": "eeaa1a01b7fb2c0d5808",
+    "url": "/React-social-network/static/js/2.9acfbb10.chunk.js"
+  },
+  {
+    "revision": "263fab3169b209f43bb3",
+    "url": "/React-social-network/static/js/main.a32ea9f7.chunk.js"
+  },
+  {
+    "revision": "8c060b7c9a3b86692c25",
+    "url": "/React-social-network/static/js/runtime~main.f216de3d.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/React-social-network/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "ec4f02e35a33d418552b8d3ad7c944a3",
+    "url": "/React-social-network/static/media/user.ec4f02e3.png"
+  }
+]);
