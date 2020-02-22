e2f463656b80f957e3af4d51c1622bdac74ec055
Alex <failemotions95@gmail.com>
2020-02-22 23:01:19 +0200

    sidebar and users reducer were modified to TypeScript

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,42 +1,12 @@
 import {profileAPI, usersAPI} from "../api/api";
 import {stopSubmit} from "redux-form";
+import { PostsType, ProfileType, PhotosType } from "../types/types";
 
 const ADD_POST = 'ADD-POST';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
 const SET_STATUS = 'SET_STATUS';
 const DELETE_POST = 'DELETE_POST';
 const SAVE_PHOTO_SUCCESS = 'SAVE_PHOTO_SUCCESS';
-
-type PostsType = {
-    id: number, 
-    message: string,
-    likesCount: number
-}
-
-type ContactsType = {
-    github: string,
-    vk: string,
-    facebook: string,
-    instagram: string,
-    twitter: string, 
-    website: string,
-    youtube: string,
-    mainLink: string
-}
-type PhotosType = {
-    small: string | null,
-    large: string | null
-}
-
-
-type ProfileType = {
-    userId: number,
-    lookingForAJob: boolean,
-    lookingForAJobDescription: string,
-    fullname: string,
-    contacts: ContactsType,
-    photos: PhotosType
-}
 
 let initialState = {
     posts: [
