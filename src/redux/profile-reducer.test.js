207ec4590e6023b8a084bf6b6bf6625349f67f5f
unknown <failemotions95@gmail.com>
2020-02-26 14:28:55 +0200

    more tests

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -35,3 +35,29 @@ it("message of new post should be correct", () => {
     // 3. expectation 
     expect(newState.posts[4].message).toBe("some text here");
 });
+
+it('after deleting length of messages should be decrement', () => {
+        // 1. test data
+        let action = deletePost(1);
+    
+        // 2. action
+        let newState = profileReducer(state, action);
+    
+       // 3. expectation
+        expect(newState.posts.length).toBe(3);
+    });
+    
+    it(`after deleting length shouldn't be decrement if id is incorrect`, () => {
+        // 1. test data
+        let action = deletePost(1000);
+    
+        // 2. action
+        let newState = profileReducer(state, action);
+    
+        // 3. expectation
+        expect(newState.posts.length).toBe(4);
+    });
+    
+    
+    
+    
