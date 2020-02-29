0e0b998a7a21388e3c05b5951339267a41c6dd8a
Alex <failemotions95@gmail.com>
2020-02-29 19:44:43 +0200

    App and index js were fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import {Route, withRouter} from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
-import {connect} from "react-redux";
-import {compose} from "redux";
-import {initializeApp} from "./redux/app-reducer";
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { initializeApp } from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/redux-store"
 
 class App extends Component {
     componentDidMount() {
@@ -19,25 +22,25 @@ class App extends Component {
 
     render() {
         if (!this.props.initialized) {
-            return <Preloader/>
+            return <Preloader />
         }
 
         return (
             <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
+                <HeaderContainer />
+                <Navbar />
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs'
-                           render={() => <DialogsContainer/>}/>
+                        render={() => <DialogsContainer />} />
 
                     <Route path='/profile/:userId?'
-                           render={() => <ProfileContainer/>}/>
+                        render={() => <ProfileContainer />} />
 
                     <Route path='/users'
-                           render={() => <UsersContainer pageTitle = {"Samurai"}/>}/>
+                        render={() => <UsersContainer pageTitle={"Samurai"} />} />
 
                     <Route path='/login'
-                           render={() => <LoginPage/>}/>
+                        render={() => <LoginPage />} />
                 </div>
             </div>
         )
@@ -48,6 +51,18 @@ const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 })
 
-export default compose(
+let AppContainer = compose(
     withRouter,
-    connect(mapStateToProps, {initializeApp}))(App);
+    connect(mapStateToProps, { initializeApp }))(App);
+
+const SamuraiApp = (props) => {
+    return (
+        <HashRouter>
+            <Provider store={store}>
+                <AppContainer />
+            </Provider>
+        </HashRouter>
+    )
+}
+
+export default SamuraiApp
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import * as serviceWorker from './serviceWorker';
-import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {HashRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import SamuraiApp from './App';
 
-ReactDOM.render(
-    <HashRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </HashRouter>, document.getElementById('root'));
+ReactDOM.render(<SamuraiApp/>
+    , document.getElementById('root'));
 
 
 // API
