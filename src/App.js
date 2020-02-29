8296a3c61022808ac104addf70dba8b70c9e0c1e
Alex <failemotions95@gmail.com>
2020-02-29 21:25:45 +0200

    lazy loading and suspense added to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Route, withRouter } from "react-router-dom";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import { connect } from "react-redux";
@@ -14,6 +11,12 @@ import Preloader from "./components/common/Preloader/Preloader";
 import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/redux-store"
+import {withSuspense} from "./hoc/withSuspense"
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+
 
 class App extends Component {
     componentDidMount() {
@@ -30,14 +33,15 @@ class App extends Component {
                 <HeaderContainer />
                 <Navbar />
                 <div className='app-wrapper-content'>
+
                     <Route path='/dialogs'
-                        render={() => <DialogsContainer />} />
+                        render={withSuspense(DialogsContainer)} />
 
                     <Route path='/profile/:userId?'
-                        render={() => <ProfileContainer />} />
+                         render={withSuspense(ProfileContainer)} />
 
                     <Route path='/users'
-                        render={() => <UsersContainer pageTitle={"Samurai"} />} />
+                        render={withSuspense(UsersContainer)} />
 
                     <Route path='/login'
                         render={() => <LoginPage />} />
