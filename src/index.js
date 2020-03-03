22f39d26eaea1f116b4543afa4b49ddd2516bc73
Alex <failemotions95@gmail.com>
2020-03-03 14:10:33 +0200

    icon was deleted

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as serviceWorker from './serviceWorker';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -7,9 +7,3 @@ import SamuraiApp from './App';
 ReactDOM.render(<SamuraiApp/>
     , document.getElementById('root'));
 
-
-// API
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
